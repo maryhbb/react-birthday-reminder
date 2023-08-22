94b24c8701ced8a49dc6f614f026d5ea6ad0b533
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-22 21:33:55 +0200

    Add feature: Display today's birthdays in List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import List from "./components/List";
 import "./index.css";
+import data from "./components/data";
+
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <>
-      <List />
+      <main>
+        <section className="container">
+          <h3>{data.length} birthday today</h3>
+          <List people={people} />
+          <button
+            onClick={() => {
+              setPeople([]);
+            }}
+          >
+            Clear All
+          </button>
+        </section>
+      </main>
     </>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,20 @@
-const List = () => {
-    return ( <h2>List component</h2> );
-}
- 
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map(person => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name}></img>
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
 export default List;
